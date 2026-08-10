4a13e3294cf4c9f4bf65aa5d7a95dbd0dfb91060
rs9100448 <rs9100448@gmail.com>
2026-08-10 12:47:13 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+    var sDict = [Character: Int]()
+    for sChar in s {
+        sDict[sChar, default: 0] += 1
+    }
+    
+    var tDict = [Character: Int]()
+    for tChar in t {
+        tDict[tChar, default: 0] += 1
+    }
+
+    return tDict == sDict
+    }
+}
