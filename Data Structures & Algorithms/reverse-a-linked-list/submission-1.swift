41951be594534e29b3c424337c95abcd103b4c80
rs9100448 <rs9100448@gmail.com>
2026-08-16 23:34:34 +0530

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.swift
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     var val: Int
+ *     var next: ListNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var pre: ListNode?
+        var cur: ListNode? = head
+        while cur != nil {
+            let next = cur?.next
+            cur?.next = pre
+            pre = cur
+            cur = next
+        }
+        return pre
+        }
+    }
+
