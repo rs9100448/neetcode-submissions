67669ce4e54c9fb41b3e8ee7baae971b4e6d3e1a
rs9100448 <rs9100448@gmail.com>
2026-09-17 08:04:11 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var l = 0
+        var r = nums.count-1
+        while l <= r {
+            let mid = l + (r-l)/2
+            print(nums[mid])
+            if nums[mid] == target {
+                return mid
+            }else if nums[mid] > target{
+                r = mid-1
+            }else {
+                l = mid+1
+            }
+        }
+        return -1
+    }
+}
